c9faeb63dba582dad3195762725a2db8e7ca7f41
BrianDev07 <brayi25.bf@gmail.com>
2025-05-12 20:42:09 +0200

    Program can now be called via CLI with arguments

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,25 @@ import (
 	utilities "background-remover/pkg"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	const (
-		imageFolder = "./input_images"  // location of original images
-		outFolder   = "./output_images" // output folder for images
-		threshold   = uint8(128)        // controls black and white levels
-	)
+	if len(os.Args) != 4 {
+		fmt.Println("Not enough arguments: <input_folder> <output_folder> <mode>")
+		os.Exit(1)
+	}
+
+	const threshold = uint8(128)
+
+	imageFolder := os.Args[1] // location of the original images
+	outFolder := os.Args[2]   // output folder for images
+
+	// controls whether the output image keeps original color or not
+	mode, err := strconv.ParseBool(os.Args[3])
+	if err != nil {
+		panic(err)
+	}
 
 	dirFiles := utilities.GetFiles(imageFolder)
 	if len(dirFiles) == 0 {
@@ -28,7 +39,7 @@ func main() {
 
 		defer baseImage.Close()
 
-		noBackgroundImage := utilities.Transform(baseImage, threshold, nil)
+		noBackgroundImage := utilities.Transform(baseImage, threshold, mode)
 		utilities.SaveImageToFile(file, noBackgroundImage, outFolder)
 	}
 }
